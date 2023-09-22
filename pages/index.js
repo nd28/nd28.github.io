1a7fba5539f1c7e29f11423ec25a87672b44b1b6
Nilesh <56911917+nd28@users.noreply.github.com>
2023-09-22 20:12:56 +0000

    Update 5 files

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import { Container, Box } from "@chakra-ui/react";
+import { Container, Box, Heading } from "@chakra-ui/react";
 
 const Page = () => {
   return (
     <Container>
       <Box borderRadius="lg" p={3} mb={6} bg="red" align="center">
         Hello, I&apos;m a full-stack developer based in India!
+      </Box>
+
+      <Box display={{ md: "flex" }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Nilesh Suthar
+          </Heading>
+          <p>( Developer / Designer )</p>
+        </Box>
       </Box>
     </Container>
   );
